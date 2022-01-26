4c1315a466bf6711db3874cf4c7f2865ea2cd6dc
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2022-01-26 09:46:19 +0100

    gitignore update

--- kwextract/TfIDFExtractor.py
+++ kwextract/TfIDFExtractor.py
@@ -1,26 +1,37 @@
 import numpy as np
+from collections import Counter
+import nltk
 
 class TFIDF:
-    def create_dtm(self, texts, cut_first=200, min_freq=3):
+    def __init__(self, df, groups):
+        self.df = df
+        self.groups = groups
 
-        # Clean texts
-        self.texts = {k: (v) for k, v in texts.items()}
+        self.texts = {k: list(v)[0] for k, v in list(pd.DataFrame((self.df.groupby(self.groups)["text"].apply(list))).iterrows())}
+
+
+    def create_ngrams(self, l, add_ngrams):
+        return ["_".join(n) for i in add_ngrams for n in nltk.ngrams(l, n=i) ]
+
+    def create_dtm(self, cut_first=200, min_freq=3, add_ngrams=None):
 
         # Count texts
-        self.vocab = nltk.FreqDist(sum(self.texts.values(), []))
+        if add_ngrams is not None:
+            self.texts = {k:v+self.create_ngrams(v,add_ngrams) for k, v in self.texts.items()}
+        self.vocab = Counter(sum(self.texts.values(),[]))
 
         # Prune overall vocabulary
         self.vocab = sorted(list(self.vocab.items()), key=lambda x: -x[1])
-        self.vocab = [x[0] for x in self.vocab[200:] if x[1] >= min_freq]
+        self.vocab = [x[0] for x in self.vocab[cut_first:] if x[1] >= min_freq]
 
         # count and restrict domain level text to the vocabulary
-        self.term_frequencies = {genre: nltk.FreqDist(text) for genre, text in self.texts.items()}
+        self.term_frequencies = {group: Counter(text) for group, text in self.texts.items()}
         self.dtm = np.array([[v.get(w, 0) for w in self.vocab] for k, v in self.term_frequencies.items()])
 
     def tfidf(self):
         tf = np.log(self.dtm + 1e-25)
         # tf = 0.5 + 0.5 * self.dtm / self.dtm.max(-1, keepdims=True) # alternative normalization.
-        idf = np.log(self.dtm.shape[0] / ((da.dtm > 0).sum(0) + 1e-25))
+        idf = np.log(self.dtm.shape[0] / ((self.dtm > 0).sum(0) + 1e-25))
         return tf * idf
 
     def tfidf_keywords(self, n=10):
@@ -46,7 +57,7 @@ def read_content(z, pdf):
     return " ".join(content)
 content = []
 with zipfile.ZipFile(path) as zf:
-    for fname in tqdm(zf.namelist()[:50]):
+    for fname in tqdm(zf.namelist()):
         try:
             content.append((fname,read_content(zf, fname)))
         except:
@@ -56,7 +67,6 @@ df = pd.DataFrame(content, columns=["file", "text"])
 
 def get_sections(x):
     try:
-        print(x)
         x_split = re.split(r"\n[Teil]{0,4}§? ?[A-Z0-9]{1,5}.? ?\n(.{4,25})\n", x) #|
         x_split = sum([re.split(r"\n[1-9IV]{1,2}.? ([a-zA-ZäöüÖÄÜ, ]{1,50})\n", t) for t in x_split],[])
         paragraphs = [("Titel", x_split[0])]
@@ -65,12 +75,14 @@ def get_sections(x):
     except:
         pass
 
-df.groupby("file").apply(lambda x:get_sections(x["text"].to_list()[0]))  # Problems 16,17,21  #|(\n[1-9]* .*\n) one line!
+df = df.groupby("file").apply(lambda x:get_sections(x["text"].to_list()[0]))  # Problems 16,17,21  #|(\n[1-9]* .*\n) one line!
+df = df.reset_index()
 
-df.file[16]
+df.columns = ["file", "-", "paragraph", "text"]
+del df["-"]
+df["text"] = df["text"].str.split()
+df = df.explode("text")
 
-nlp = spacy.load("en_core_web_sm")
-
-df["text"].map(lambda x: list(nlp(x)))
-nlp(content)
-pd.DataFrame({x[0]:list(nlp.pipe(x[1])) for x in content})
+kw = TFIDF(df=df, groups=["file"])
+kw.create_dtm(add_ngrams=[2,3])
+kw.tfidf_keywords(100)
