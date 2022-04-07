21778b7f43b3feb306f738616ec1ccd8573ab815
Janos Borst <mai13ivw@studserv.uni-leipzig.de>
2022-04-07 18:32:39 +0200

    debug file

--- debug.py
+++ debug.py
@@ -6,10 +6,10 @@ with zipfile.ZipFile("/home/jb/Downloads/keyword.zip") as zfile:
         if f != "keyword/":
             content[f] = zfile.read(f).decode("utf8")
 content.keys()
-# path = "/home/jb/Downloads/deu_news_2020_1M-sentences.txt"
-# with open(path, "r") as f:
-#     reference = [" ".join(x.split("\t")[1:]).strip() for x in  f.readlines()]
-# content["reference"] = " ".join(reference[:100])
+path = "/home/jb/Downloads/deu_news_2020_1M-sentences.txt"
+with open(path, "r") as f:
+    reference = [" ".join(x.split("\t")[1:]).strip() for x in  f.readlines()]
+content["reference"] = " ".join(reference[:100])
 
 content = {k:v.split(" ") for k, v in content.items()}
 
@@ -22,5 +22,5 @@ from kwextract import TFIDF, LLH
 # kw = LLH( pos_pattern=[["DET", "ADJ", "Noun"], ["VERB", "ADV"], ["NOUN","PROPN", "NOUN"], ["NOUN", "NOUN"]])
 # kw(df, groups=["key"],n=100)
 
-kw = TFIDF()
+kw = LLH()
 kw.call_dict(content, 10)
